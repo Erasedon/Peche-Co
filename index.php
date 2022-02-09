29d92a6326419dd46cc921be471728b3bbd12565
JulienGenonceau <juliengenonceau@hotmail.com>
2022-02-09 15:48:55 +0100

    fini

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="./asset/style/style.css"/>
+</head>
+<body>
+    <?php include("slider.php");?>
+    <?php include("parallax.php");?>
+    <?php include("scriptjs.php");?>
+</body>
+</html>
