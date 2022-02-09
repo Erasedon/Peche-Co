cb73fb95fe1064ddaeac1ef2cb6394371473cde4
Spacantho <98018883+Spacantho@users.noreply.github.com>
2022-02-09 16:29:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,11 @@
 </head>
 <body>
     <?php include("assets/includes/navbar.php"); ?>
-
+    <?php include("assets/includes/slider.php"); ?>
+    <?php include("assets/includes/section1.php"); ?>
+    <?php include("assets/includes/parallax.php"); ?>
+    <?php include("assets/includes/footer.php"); ?>
+    <?php include("assets/includes/scriptjs.php"); ?>
     <script src="assets/js/navbar.js"></script>
 </body>
 </html>
